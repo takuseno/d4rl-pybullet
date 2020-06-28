6dd4c7eacefb6cfed98aa6cf4c8ad5b1910e8b29
takuseno <takuma.seno@gmail.com>
2020-06-28 23:43:14 +0900

    Add halfcheetah registration

--- d4rl_pybullet/__init__.py
+++ d4rl_pybullet/__init__.py
@@ -23,3 +23,27 @@ register(
         'dataset_url':
         'https://www.dropbox.com/s/fz1u9nak9z1sa9y/hopper-bullet-random-v0.hdf5?dl=1'
     })
+
+register(
+    id='halfcheetah-bullet-medium-v0',
+    entry_point='d4rl_pybullet.envs:OfflineHalfCheetahBulletEnv',
+    kwargs={
+        'dataset_url':
+        'https://www.dropbox.com/s/wdo6lktqgyqztfk/halfcheetah-bullet-medium-v0.hdf5?dl=1'
+    })
+
+register(
+    id='halfcheetah-bullet-mixed-v0',
+    entry_point='d4rl_pybullet.envs:OfflineHalfCheetahBulletEnv',
+    kwargs={
+        'dataset_url':
+        'https://www.dropbox.com/s/ne23j1jdoks240g/halfcheetah-bullet-mixed-v0.hdf5?dl=1'
+    })
+
+register(
+    id='halfcheetah-bullet-random-v0',
+    entry_point='d4rl_pybullet.envs:OfflineHalfCheetahBulletEnv',
+    kwargs={
+        'dataset_url':
+        'https://www.dropbox.com/s/m30chvy08vg4dou/halfcheetah-bullet-random-v0.hdf5?dl=1'
+    })
