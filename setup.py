b9e9ba430710b7428495f8013b13ebea345f8218
takuseno <takuma.seno@gmail.com>
2020-06-28 22:05:33 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name="d4rl_pybullet",
+      version="0.1",
+      license="MIT",
+      description="Datasets for data-driven deep reinforcement learnig with Pybullet environments",
+      url="https://github.com/takuseno/d4rl-pybullet",
+      install_requires=["torch", "gym", "pybullet", "h5py"],
+      packages=find_packages())
