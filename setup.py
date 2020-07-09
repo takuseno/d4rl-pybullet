1f211410b76e26c43535484f7a081414cde509fe
takuseno <takuma.seno@gmail.com>
2020-07-09 09:19:55 +0900

    Restructure test directory

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(name="d4rl_pybullet",
       description="Datasets for data-driven deep reinforcement learnig with Pybullet environments",
       url="https://github.com/takuseno/d4rl-pybullet",
       install_requires=["gym", "pybullet", "h5py"],
-      packages=find_packages())
+      packages=find_packages("d4rl_pybullet"),
+      package_dir={"": "d4rl_pybullet"})
