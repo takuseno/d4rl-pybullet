13df279459738370b140a28e862191cc3ff3b785
takuseno <takuma.seno@gmail.com>
2020-07-09 17:05:46 +0900

    Set max_episode_steps as original environments

--- d4rl_pybullet/__init__.py
+++ d4rl_pybullet/__init__.py
@@ -3,6 +3,7 @@ from gym.envs.registration import register
 register(
     id='hopper-bullet-medium-v0',
     entry_point='d4rl_pybullet.envs:OfflineHopperBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/w22kgzldn6eng7j/hopper-bullet-medium-v0.hdf5?dl=1'
@@ -11,6 +12,7 @@ register(
 register(
     id='hopper-bullet-mixed-v0',
     entry_point='d4rl_pybullet.envs:OfflineHopperBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/xv3p0h7dzgxt8xb/hopper-bullet-mixed-v0.hdf5?dl=1'
@@ -19,6 +21,7 @@ register(
 register(
     id='hopper-bullet-random-v0',
     entry_point='d4rl_pybullet.envs:OfflineHopperBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/bino8ojd7iq4p4d/hopper-bullet-random-v0.hdf5?dl=1'
@@ -27,6 +30,7 @@ register(
 register(
     id='halfcheetah-bullet-medium-v0',
     entry_point='d4rl_pybullet.envs:OfflineHalfCheetahBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/v4xgssp1w968a9l/halfcheetah-bullet-medium-v0.hdf5?dl=1'
@@ -35,6 +39,7 @@ register(
 register(
     id='halfcheetah-bullet-mixed-v0',
     entry_point='d4rl_pybullet.envs:OfflineHalfCheetahBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/scj1rqun963aw90/halfcheetah-bullet-mixed-v0.hdf5?dl=1'
@@ -43,6 +48,7 @@ register(
 register(
     id='halfcheetah-bullet-random-v0',
     entry_point='d4rl_pybullet.envs:OfflineHalfCheetahBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/jnvpb1hp60zt2ak/halfcheetah-bullet-random-v0.hdf5?dl=1'
@@ -51,6 +57,7 @@ register(
 register(
     id='ant-bullet-medium-v0',
     entry_point='d4rl_pybullet.envs:OfflineAntBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/6n79kwd94xthr1t/ant-bullet-medium-v0.hdf5?dl=1'
@@ -59,6 +66,7 @@ register(
 register(
     id='ant-bullet-mixed-v0',
     entry_point='d4rl_pybullet.envs:OfflineAntBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/pmy3dzab35g4whk/ant-bullet-mixed-v0.hdf5?dl=1'
@@ -67,6 +75,7 @@ register(
 register(
     id='ant-bullet-random-v0',
     entry_point='d4rl_pybullet.envs:OfflineAntBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/2xpmh4wk2m7i8xh/ant-bullet-random-v0.hdf5?dl=1'
@@ -75,6 +84,7 @@ register(
 register(
     id='walker2d-bullet-medium-v0',
     entry_point='d4rl_pybullet.envs:OfflineWalker2DBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/v0f2kz48b1hw6or/walker2d-bullet-medium-v0.hdf5?dl=1'
@@ -83,6 +93,7 @@ register(
 register(
     id='walker2d-bullet-mixed-v0',
     entry_point='d4rl_pybullet.envs:OfflineWalker2DBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/i4u2ii0d85iblou/walker2d-bullet-mixed-v0.hdf5?dl=1'
@@ -91,6 +102,7 @@ register(
 register(
     id='walker2d-bullet-random-v0',
     entry_point='d4rl_pybullet.envs:OfflineWalker2DBulletEnv',
+    max_episode_steps=1000,
     kwargs={
         'dataset_url':
         'https://www.dropbox.com/s/1gwcfl2nmx6878m/walker2d-bullet-random-v0.hdf5?dl=1'
